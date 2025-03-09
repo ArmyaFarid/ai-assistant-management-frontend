027a7cc9fd5b500d23f7e45edcd58e3d27f99a04
cleanCode_dev <armyabakouan@icloud.com>
2025-03-09 16:45:32 +0000

    format env

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,6 +1,4 @@
 export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://srv645133.hstgr.cloud:9090' : 'http://localhost:9090' ;
 export const API_BASE_URL = BASE_URL+"/api"
-
-
 export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#_/$%^&*])[A-Za-z\d!@_/#$%^&*]{8,15}$/;
 
